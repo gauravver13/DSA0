2e0c487d009112d6903b9ae39003bd3be12f5bec
Gaurav Verma <100416827+gauravver13@users.noreply.github.com>
2022-11-25 23:59:19 +0530

    Add files via upload

---
+++ recursion/rec1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+//  head recurssion function
+
+void fun(int n){
+    
+    if(n>0)
+    {
+    fun(n-1);
+    cout<<n<<" ";
+    }
+}
+
+int main(){
+    int x=3;
+
+    fun(x);
+    return 0;
+}
